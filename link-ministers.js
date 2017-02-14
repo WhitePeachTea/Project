25e5e88ae212a795b8e2f44932214a23ca2ccbfd
WhitePeachTea <peng_lei_1021@hotmail.com>
2017-02-14 09:40:29 -0500

    Update link-ministers.js

--- link-ministers.js
+++ link-ministers.js
@@ -1,24 +1,36 @@
 // I use this helper function to create <a> tags inside a given node
 function addLink(node, text, url) {
     // clear the node of text; create a new "a" node; add the appropriate attributes;
-    // append the new node as a child; return the original node.  
-    return node;
+    // append the new node as a child; return the original node.
+var url = "https://en.Wikipedia.org/wiki//" + text;{
+var node = document.getElementsByClassName(node);
+var text = node.textContent;
+node.textContent = "";
 }
-
+var link = document.createElement("a");
+var newnode = document.createTextNode(url);
+link.appendChild(newnode);
+var element = document.getElementsByClassName(node);
+element.appendChild(link);
+return node;
+}
 // another helper function, if you want to replace spaces w/ "_"
-function wikify(text) {
+//function wikify(text) {
+
     // replace spaces w/ "_"; concatenate w/ Wikipedia prefix; return concatenated string
-    return;
-}
-
-
-// take a class name as parameter, and linkify all such classes.  
+    //return;}
+// take a class name as parameter, and linkify all such classes.
 function linkifyClass (c) {
-    // get all elements of class c; loop through elements;
-    // add link to each element
-    
-    // no need for a return value. 
+var c1 = document.getElementsByClassName(c);
+for (var a = 0; a < c1.length; a++){
+  console.log (addLink (c1[a]));}
 }
+  // get all elements of class c; loop through elements;
+    // add link to each element
+
+    // no need for a return value.
+
+
 
 // now call the function so that the work actually gets done.  
 linkifyClass("PM");
